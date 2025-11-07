23cddebacae86008dc9740e4daeb4a20f3f75e52
Alirie Gray <alirie.gray@gmail.com>
2025-11-07 15:55:30 -0800

    wip

--- Comp584Server/obj/Debug/net9.0/Comp584Server.AssemblyInfo.cs
+++ Comp584Server/obj/Debug/net9.0/Comp584Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Comp584Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd95b8dcbdab290ecf495ab4dda72a5b09111ea1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e5f6aa8de56ae3cbbd7d8ac1653400df62b02ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("Comp584Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Comp584Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
